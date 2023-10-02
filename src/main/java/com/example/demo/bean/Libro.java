c3334785dff50413b614bd77f87ed691efd7b160
Jostin Fabian <Jostinfabian14@gmail.com>
2023-10-02 19:32:37 +0200

    Modificamos el modelo libro para poder acceder a sus propiedades

--- src/main/java/com/example/demo/bean/Libro.java
+++ src/main/java/com/example/demo/bean/Libro.java
@@ -7,6 +7,7 @@ public class Libro {
     private String editorial;
     private String fecha;
     private String tematica;
+
     public Libro(int id, String titulo, String autor, String editorial, String fecha, String tematica) {
         this.id = id;
         this.titulo = titulo;
@@ -15,4 +16,53 @@ public class Libro {
         this.fecha = fecha;
         this.tematica = tematica;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getTematica() {
+        return tematica;
+    }
+
+    public void setTematica(String tematica) {
+        this.tematica = tematica;
+    }
+
 }
