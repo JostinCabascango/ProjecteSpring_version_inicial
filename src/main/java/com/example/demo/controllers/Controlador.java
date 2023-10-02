fe8a5136cf642f126ecd3173f1fd8d7fe1a8b783
Jostin Fabian <Jostinfabian14@gmail.com>
2023-10-02 16:20:29 +0200

    Creado mi primer controlador

--- src/main/java/com/example/demo/controllers/Controlador.java
+++ src/main/java/com/example/demo/controllers/Controlador.java
@@ -1,4 +1,19 @@
 package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class Controlador {
-    
+    @GetMapping("/")
+    public String iniciar() {
+        return "login";
+    }
+
+    @PostMapping("")
+    public String login() {
+        return "consulta";
+    }
+
 }
