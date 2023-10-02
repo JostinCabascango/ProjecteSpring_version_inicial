78764ab7dc45103735339ed755911fb64bde297e
Jostin Fabian <Jostinfabian14@gmail.com>
2023-10-02 19:34:19 +0200

    Modificamos consulta.html y Controlador.java para poder lista los libros en la pagina web

--- src/main/java/com/example/demo/controllers/Controlador.java
+++ src/main/java/com/example/demo/controllers/Controlador.java
@@ -1,24 +1,33 @@
 package com.example.demo.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.bean.Usuario;
+import com.example.demo.bean.Libro;
+
+import repository.BaseDatos;
 
 @Controller
 public class Controlador {
+    BaseDatos bd = new BaseDatos();
+
     @GetMapping("/")
     public String iniciar(Model model) {
         model.addAttribute("titulo", "FORMULARIO DE ACCESO");
         return "login";
     }
 
-    @PostMapping("")
+    @PostMapping("/")
     public String login(Model model, Usuario usuario) {
         if (usuario.getNombre().equals("edu") && usuario.getPassword().equals("edu")) {
+            ArrayList<Libro> libros = bd.getLibros();
+            model.addAttribute("usuario", usuario);
+            model.addAttribute("libros", libros);
             return "consulta";
         } else {
             return "login";
