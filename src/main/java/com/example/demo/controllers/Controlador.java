6ffe91d0abc51bd1253b820b2c1e61a4a422e8a7
Jostin Fabian <Jostinfabian14@gmail.com>
2023-10-02 17:51:32 +0200

    Integrar Thymeleaf en mis controladores y templates

--- src/main/java/com/example/demo/controllers/Controlador.java
+++ src/main/java/com/example/demo/controllers/Controlador.java
@@ -1,19 +1,29 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.bean.Usuario;
 
 @Controller
 public class Controlador {
     @GetMapping("/")
-    public String iniciar() {
+    public String iniciar(Model model) {
+        model.addAttribute("titulo", "FORMULARIO DE ACCESO");
         return "login";
     }
 
     @PostMapping("")
-    public String login() {
-        return "consulta";
+    public String login(Model model, Usuario usuario) {
+        if (usuario.getNombre().equals("edu") && usuario.getPassword().equals("edu")) {
+            return "consulta";
+        } else {
+            return "login";
+
+        }
     }
 
 }
