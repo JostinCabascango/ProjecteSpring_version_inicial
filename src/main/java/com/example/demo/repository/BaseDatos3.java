7c1a60c5ec93155a1f4e59bafd607889c430b2fc
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2023-10-03 17:23:55 +0200

    PRUEBA REALIZADA CON JPA

---
+++ src/main/java/com/example/demo/repository/BaseDatos3.java
@@ -0,0 +1,4 @@
+package com.example.demo.repository;
+
+public interface BaseDatos3 extends org.springframework.data.jpa.repository.JpaRepository<com.example.demo.bean.Libro, java.lang.Integer> {
+}
